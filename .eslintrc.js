1dfe7229fc932c98009f2edd6d9ccd5ab0d46d8b
wahyukurniaa <wahyukurnia321123@gmail.com>
2022-12-01 18:36:41 +0700

    Add Eslint, Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "standard-with-typescript",
+    "extends",
+    ["next/core-web-vitals", "prettier"],
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": 0,
+  },
+};
